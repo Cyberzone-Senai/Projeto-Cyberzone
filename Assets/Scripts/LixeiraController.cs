dc85ddf6fc15b88106062c5d09417d1484a33e93
EllenFerreira18 <ellen.github@gmail.com>
2024-12-09 16:59:48 -0300

    feat: lixeira dano

---
+++ Assets/Scripts/LixeiraController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class LixeiraController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public bool isDead;
+
+    public int maxHealth;
+    public int currentHealth;
+
+    public bool isTakingDamage;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        currentHealth = maxHealth;
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage (int  damage)
+    {
+        if (!isDead)
+        {
+            isTakingDamage = true;
+            currentHealth -= damage;
+
+            animator.SetTrigger("Lixeira_Quebrada");
+
+        }
+    }
+
+    public void DisableEnemy()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
